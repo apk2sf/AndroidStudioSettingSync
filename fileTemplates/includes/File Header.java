0355cd5297ed0b030aa4919d24c43ca8c98e8c13
upuptop <pyfysf@163.com>
2019-12-12 00:36:54 +0800

    AI-3.5.3 <upuptop@UpUpTop-PC Create Vibrant Unknown (Darcula).icls, ide.general.xml, other.xml, git.xml, project.default.xml, Singleton.java, Enum.java, Default copy.xml, Interface.java, Default.xml, androidStudioFirstRun.xml, file.template.settings.xml, find.xml, ui.lnf.xml, File Header.java, Default_shaofei.xml, Default copy.icls, keymap.xml, AndroidComments.xml, code.style.schemes.xml, plugin_ui.xml, AndroidLog.xml, colors.scheme.xml, Darculashaofei.icls, notifications.xml, Darcula copy.icls, templates.xml, AnnotationType.java, Sublime Monokai True.icls, Eclipse_shaofei.xml, output.xml, JavaScript.xml, laf.xml, Vividchalk.icls, AndroidXML.xml, FadeComments.icls, Class.java, jdk.table.xml, path.macros.xml, Eclipse copy.xml, editor.codeinsight.xml, Android.xml, toStringTemplates.xml, editor.xml, debugger.xml, updates.xml

---
+++ fileTemplates/includes/File Header.java
@@ -0,0 +1,13 @@
+/**
+ * 作者：  pyfysf
+ * <p>
+ * qq:  337081267
+ * <p>
+ * CSDN:    http://blog.csdn.net/pyfysf
+ * <p>
+ * 个人博客：    http://wintp.top
+ * <p>
+ * 时间： ${YEAR}/${MONTH}/${DATE} ${TIME}
+ * <p>
+ * 邮箱：  pyfysf@163.com
+ */
